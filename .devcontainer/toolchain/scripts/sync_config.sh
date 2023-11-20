d0000157075245d02035f50468f2b8dd3ce82cf7
Samy Francelet <samy.francelet@ik.me>
2023-11-20 12:12:07 +0100

    Patched sync_config.sh

--- .devcontainer/toolchain/scripts/sync_config.sh
+++ .devcontainer/toolchain/scripts/sync_config.sh
@@ -1,2 +1,9 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
 rsync -a /workspace/config/board/ /buildroot/board/
 rsync -a /workspace/config/configs/ /buildroot/configs/
