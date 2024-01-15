266b21ba01eea9bcb8570a394e7f7dcff0343b0b
Francelet Samy <samy.francelet@ik.me>
2024-01-15 18:01:16 +0100

    fixed luks + added initramfs

--- config/board/friendlyarm/nanopi-neo-plus2/boot.cmd
+++ config/board/friendlyarm/nanopi-neo-plus2/boot.cmd
@@ -1,5 +1,7 @@
 setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
 
-ext4load mmc 0:1 0x40000000 Image.itb
+ext4load mmc 0 $kernel_addr_r Image
+ext4load mmc 0 $fdt_addr_r sun50i-h5-nanopi-neo-plus2.dtb
+ext4load mmc 0 0x50000000 uInitrd
 
-bootm 0x40000000
+booti $kernel_addr_r 0x50000000 $fdt_addr_r
--- config/board/friendlyarm/nanopi-neo-plus2/post-build.sh
+++ config/board/friendlyarm/nanopi-neo-plus2/post-build.sh
@@ -3,8 +3,8 @@ BOARD_DIR="$(dirname $0)"
 BUILDROOT_DIR="/buildroot"
 IMAGES_DIR="/buildroot/output/images"
 
-cp $BOARD_DIR/kernel_fdt.its $IMAGES_DIR
-mkimage -f $IMAGES_DIR/kernel_fdt.its -E $IMAGES_DIR/Image.itb
+# cp $BOARD_DIR/kernel_fdt.its $IMAGES_DIR
+# mkimage -f $IMAGES_DIR/kernel_fdt.its -E $IMAGES_DIR/Image.itb
 
 mkimage -C none -A arm64 -T script -d $BOARD_DIR/boot.cmd $IMAGES_DIR/boot.scr
 
@@ -13,9 +13,17 @@ rm -rf $IMAGES_DIR/boot.ext4
 dd if=/dev/zero of=$IMAGES_DIR/boot.ext4 bs=1024 count=65536
 mkfs.ext4 -L boot $IMAGES_DIR/boot.ext4
 
-# Insert Image.itb and boot.scr into boot.ext4 
+# Execute initramfs.sh
+$BOARD_DIR/initramfs.sh
+
+cd /buildroot
+
+# Insert Image, dtb, uInitrd and boot.scr into boot.ext4 
 mount -o loop $IMAGES_DIR/boot.ext4 /mnt
-cp $IMAGES_DIR/Image.itb /mnt
+#cp $IMAGES_DIR/Image.itb /mnt
+cp $IMAGES_DIR/Image /mnt
+cp $IMAGES_DIR/sun50i-h5-nanopi-neo-plus2.dtb /mnt
+cp $IMAGES_DIR/uInitrd /mnt
 cp $IMAGES_DIR/boot.scr /mnt
 umount /mnt
 
@@ -28,9 +36,13 @@ echo "YES" | cryptsetup --pbkdf pbkdf2 luksFormat $IMAGES_DIR/rootfs.luks.ext4 $
 # Create mapping, format LUKS partition and insert rootfs.ext4 into LUKS image
 cryptsetup open --type luks --key-file=/$IMAGES_DIR/luks_pw $IMAGES_DIR/rootfs.luks.ext4 rootfs
 mkfs.ext4 -L luks /dev/mapper/rootfs
-mount /dev/mapper/rootfs /mnt/
-cp -r $IMAGES_DIR/rootfs.ext4 /mnt
+mkdir -p /luks/
+mkdir -p /mnt/
+mount /dev/mapper/rootfs /luks/
+mount -o loop $IMAGES_DIR/rootfs.ext4 /mnt/
+rsync -a /mnt/ /luks/
 umount /mnt/
+umount /luks/
 cryptsetup close rootfs
 
 # Generate btrfs image
