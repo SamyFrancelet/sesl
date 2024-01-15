9669b9a4f40a30630360b4c4bf6e5fba18152a0e
Francelet Samy <samy.francelet@ik.me>
2024-01-16 00:07:27 +0100

    initramfs + LUKS fully working !

--- config/board/friendlyarm/nanopi-neo-plus2/initramfs.sh
+++ config/board/friendlyarm/nanopi-neo-plus2/initramfs.sh
@@ -5,7 +5,7 @@ mkdir $RAMFS
 mkdir $RAMFS2
 cd $RAMFS
 
-mkdir -p $RAMFS2/{bin,dev,etc,home,lib,newroot,proc,root,usr/bin,usr/lib,sbin,sys}
+mkdir -p $RAMFS2/{bin,dev,etc,home,lib,newroot,proc,root,usr/bin,usr/sbin,usr/lib,sbin,sys,run}
 
 cd $RAMFS2
 cd bin
@@ -32,15 +32,43 @@ cd sbin
 ln -s ../bin/busybox switch_root
 cd $RAMFS2
 
+cd usr/sbin
+cp /buildroot/output/target/usr/sbin/cryptsetup .
+cd $RAMFS2
+
 # Copy shared libraries
 cd lib
 cp /buildroot/output/target/lib/ld-uClibc-1.0.42.so .
 cp /buildroot/output/target/lib/libuClibc-1.0.42.so .
+cp /buildroot/output/target/lib/libuuid.so.1.3.0 .
+cp /buildroot/output/target/lib/libblkid.so.1.1.0 .
+cp /buildroot/output/target/lib/libatomic.so.1.2.0 .
 ln -s ld-uClibc.so.1 ld-uClibc.so.0
 ln -s ld-uClibc-1.0.42.so ld-uClibc.so.1
 ln -s libuClibc-1.0.42.so libc.so.0
 ln -s libuClibc-1.0.42.so libc.so.1
+ln -s libuuid.so.1.3.0 libuuid.so.1
+ln -s libblkid.so.1.1.0 libblkid.so.1
+ln -s libatomic.so.1.2.0 libatomic.so.1
 cd $RAMFS2
+
+cd usr/lib
+cp /buildroot/output/target/usr/lib/libcryptsetup.so.12.8.0 .
+cp /buildroot/output/target/usr/lib/libpopt.so.0.0.1 .
+cp /buildroot/output/target/usr/lib/libdevmapper.so.1.02 .
+cp /buildroot/output/target/usr/lib/libssl.so.1.1 .
+cp /buildroot/output/target/usr/lib/libcrypto.so.1.1 .
+cp /buildroot/output/target/usr/lib/libargon2.so.1 .
+cp /buildroot/output/target/usr/lib/libjson-c.so.5.2.0 .
+cp /buildroot/output/target/usr/lib/libiconv.so.2.6.0 .
+ln -s libcryptsetup.so.12.8.0 libcryptsetup.so.12
+ln -s libpopt.so.0.0.1 libpopt.so.0
+ln -s libjson-c.so.5.2.0 libjson-c.so.5
+ln -s libiconv.so.2.6.0 libiconv.so.2
+cd $RAMFS2
+
+# Copy luks_pw into initramfs at /luks_pw
+cp /buildroot/output/images/luks_pw $RAMFS2/luks_pw
 
 # Generate /init file
 cat > init << endofinput
@@ -49,11 +77,11 @@ mount -t proc none /proc
 mount -t sysfs none /sys
 
 mount -n -t devtmpfs devtmpfs /dev
-mount -t ext4 /dev/mmcblk0p2 /newroot
+cryptsetup open --type luks --key-file=/luks_pw /dev/mmcblk0p3 luks_rootfs
+mount /dev/mapper/luks_rootfs /newroot
 mount -n -t devtmpfs devtmpfs /newroot/dev
 
-exec sh
-# exec switch_root /newroot /sbin/init
+exec switch_root /newroot /sbin/init
 endofinput
 
 # Make /init executable
--- config/board/friendlyarm/nanopi-neo-plus2/post-build.sh
+++ config/board/friendlyarm/nanopi-neo-plus2/post-build.sh
@@ -13,6 +13,8 @@ rm -rf $IMAGES_DIR/boot.ext4
 dd if=/dev/zero of=$IMAGES_DIR/boot.ext4 bs=1024 count=65536
 mkfs.ext4 -L boot $IMAGES_DIR/boot.ext4
 
+cp $BOARD_DIR/rootfs_overlay/luks_pw $IMAGES_DIR/
+
 # Execute initramfs.sh
 $BOARD_DIR/initramfs.sh
 
@@ -30,7 +32,7 @@ umount /mnt
 # Generate LUKS image
 cp $BOARD_DIR/rootfs_overlay/luks_pw $IMAGES_DIR/
 hexdump $IMAGES_DIR/luks_pw
-dd if=/dev/zero of=$IMAGES_DIR/rootfs.luks.ext4 bs=2147483648 count=1
+dd if=/dev/zero of=$IMAGES_DIR/rootfs.luks.ext4 bs=1024 count=65536
 echo "YES" | cryptsetup --pbkdf pbkdf2 luksFormat $IMAGES_DIR/rootfs.luks.ext4 $IMAGES_DIR/luks_pw
 
 # Create mapping, format LUKS partition and insert rootfs.ext4 into LUKS image
@@ -44,12 +46,3 @@ rsync -a /mnt/ /luks/
 umount /mnt/
 umount /luks/
 cryptsetup close rootfs
-
-# Generate btrfs image
-#dd if=/dev/zero of=/buildroot/output/images/cow.btrfs bs=1024 count=400k
-#mkfs.btrfs -L btrfs /buildroot/output/images/cow.btrfs
-
-# Generate f2fs image
-#dd if=/dev/zero of=/buildroot/output/images/log.f2fs bs=1024 count=400k
-#mkfs.f2fs -l f2fs /buildroot/output/images/log.f2fs
-
